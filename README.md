6cb46bf192c49f8a5f1e7a7ac9639d9c2c0e0231
Henry Cuesta <141352590+henry-cv@users.noreply.github.com>
2025-02-11 17:50:12 -0500

    Commit 1, recien creado el proyecto con vite, App.jsx limpia

---
+++ README.md
@@ -0,0 +1,1 @@
+# Repositorio del código Frontend del proyecto integrador CTD
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>App Frontend recién iniciada</h1>
+    </>
+  );
+}
+
+export default App;
