9c8061768f67094846f34063025206e98396bd82
Pedro Ponce Vergara <pedro.poncevergara@gmail.com>
2025-02-15 18:25:13 -0300

    feat: optimización de SearchBox y estilos del Home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,5 @@
 import Carousel from '../components/Carousel';
-import SearchBar from '../components/SearchBar';
+import SearchBox from '../components/SearchBox';
 
 const Home = () => {
   const images = [
@@ -11,9 +11,15 @@ const Home = () => {
   return (
     <div className="min-h-screen w-full relative">
       <Carousel images={images} />
-      <div className="relative z-10">
-        <SearchBar />
+      <div className="absolute top-1/4 left-1/2 transform -translate-x-1/2 text-center z-10">
+        <h1 className="font-['Archivo'] text-[40px] text-white font-bold mb-4 whitespace-nowrap">
+          Descubre, reserva y vive nuevas experiencias
+        </h1>
+        <h6 className="font-['Archivo'] text-base text-white">
+          Conéctate con la emoción de viajar, descubrir y disfrutar
+        </h6>
       </div>
+      <SearchBox />
       <div className="relative z-10 w-full">
         {/* Contenido que irá debajo del carousel */}
       </div>
