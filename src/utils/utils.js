7660743c9d3d7449e58eb43c738758db94967923
Henry Cuesta <141352590+henry-cv@users.noreply.github.com>
2025-02-20 01:13:09 -0500

    Feature/henry cv/actualizar valores css (#11)
    * iniciando actualización de valores css BluePill.css
    
    * Actualizadas variables css dentro de BluePill.css
    
    * Actualizados valores de css en DateCalendar, Days, FieldError
    
    * Corregido uso de variables css, y agregada carpeta utils en este commit

---
+++ src/utils/utils.js
@@ -0,0 +1,50 @@
+/* ---------------------------------- texto --------------------------------- */
+export function validarTexto(texto, maximo) {
+  texto = normalizarTexto(texto);
+  /* console.log(`Texto normalizado: ${texto}`);
+  console.log(`Texto validado: ${/^[a-záéíóúñ]{3,}$/i.test(texto)}`); */
+  const regex = new RegExp(`^[a-záéíóúñ ]{4,${maximo}}$`, 'i');
+  return regex.test(texto);
+}
+export function validarAreaTexto(texto, maximo) {
+  texto = normalizarTexto(texto);
+  /* console.log(`Texto normalizado: ${texto}`);
+  console.log(`Texto validado: ${/^[a-záéíóúñ]{3,}$/i.test(texto)}`); */
+  const regex = new RegExp(`^[\\wáéíóúñ ]{4,${maximo}}$`, 'i');
+  return regex.test(texto);
+}
+export function longitudPermitida(texto, maximo) {
+  texto = normalizarTexto(texto);
+  return texto.length <= maximo;
+}
+export function direccionMaxCaracteres(texto) {
+  texto = normalizarTexto(texto);
+  return texto.length <= 60;
+}
+export function normalizarTexto(texto) {
+  // Agrega Capitalización a texto
+  texto = texto.trim();
+  // slice(a,b), son indices a inicial, b final, excluyente
+  return texto.charAt(0).toUpperCase().concat(texto.slice(1));
+}
+
+/* ---------------------------------- email --------------------------------- */
+export function validarEmail(email) {
+  email = normalizarEmail(email);
+  // console.log(`Email: *${email}*`);
+  return /^[\w.]{4,}@[a-z]{3,}\.[a-z]{2,4}$/.test(email);
+}
+
+export function normalizarEmail(email) {
+  // console.log(`-->${email.trim()}<--`);
+  return email.trim();
+}
+
+/* -------------------------------- password -------------------------------- */
+export function validarContrasenia(contrasenia) {
+  return /^\w{6,}$/i.test(contrasenia);
+}
+
+export function compararContrasenias(contrasenia_1, contrasenia_2) {
+  return contrasenia_1 === contrasenia_2;
+}
