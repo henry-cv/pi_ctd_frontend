40b263030f88b64807cb5deb1c557c2f155ca33a
Ronald Santamaria <pizarroronald06@gmail.com>
2025-02-25 21:35:33 -0300

    Rotacion btn sidebar corregido

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -52,7 +52,7 @@ const Dashboard = () => {
           </div>
           <button
             className={`${
-              barOpen
+              !barOpen
                 ? "circleToggle shadowCircle"
                 : "circleToggle animationToggle"
             }`}
