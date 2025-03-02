013b7678ea274ab8f9b237879dedaeccbf29c70f
Mariano <marhergc@gmail.com>
2025-03-02 11:30:01 -0300

    Subiendo PanelControl y Modal

--- src/components/PanelControl.jsx
+++ src/components/PanelControl.jsx
@@ -2,37 +2,50 @@ import "../css/components/PanelControl.css";
 import PurpleHeart from '/Purpleheart.png';
 import YellowHeart from '/YellowHeart.png';
 import CataVino from '/CataVino.png';
+import ModalAviso from "./ModalAviso"; // Importar ModalAviso
+import { useState } from "react";
 
 const PanelControl = () => {
+  const [showModal, setShowModal] = useState(true); // Estado para controlar la visibilidad del modal
+
+  const handleCloseModal = () => {
+    setShowModal(false); // Ocultar el modal cuando se cierre
+  };
+
   return (
     <div className="dashboard-main">
-      {/* Título principal del Panel de Control */}
+      {/* Mostrar ModalAviso solo si showModal es true */}
+      {showModal && <ModalAviso onClose={handleCloseModal} />}
+
+      {/* Título principal */}
       <div className="dashboard-title">Panel de Control</div>
 
+      {/* Stats */}
       <div className="stats">
         <div className="stat-card">
           <img src={PurpleHeart} alt="Total Ventas" className="stat-icon" />
-          <div>
+          <div className="stat-info">
             <h3>Total Ventas</h3>
             <p>$7500k</p>
           </div>
         </div>
         <div className="stat-card">
           <img src={YellowHeart} alt="Total Reservas" className="stat-icon" />
-          <div>
+          <div className="stat-info">
             <h3>Total Reservas</h3>
             <p>100</p>
           </div>
         </div>
         <div className="stat-card">
           <img src={YellowHeart} alt="Nuevos usuarios" className="stat-icon" />
-          <div>
+          <div className="stat-info">
             <h3>Nuevos usuarios</h3>
             <p>5</p>
           </div>
         </div>
       </div>
 
+      {/* Actividades */}
       <div className="activities">
         <h2>Actividades más vendidas</h2>
         <table>
@@ -50,7 +63,7 @@ const PanelControl = () => {
                 <td>{actividad.id}</td>
                 <td className="activity-cell">
                   <img src={CataVino} alt="Cata de Vinos" className="activity-img" />
-                  Cata de Vinos
+                  <span>Cata de Vinos</span>
                 </td>
                 <td>$75</td>
                 <td>{actividad.cantidad}</td>
@@ -58,6 +71,14 @@ const PanelControl = () => {
             ))}
           </tbody>
         </table>
+      </div>
+
+      {/* Card Info - Solo en mobile */}
+      <div className="info-card">
+        <p>Para acceder a todas las funciones de administración, usa la versión de escritorio.</p>
+        <a href="https://gobook-dh.vercel.app/administrador/panel" target="_blank" rel="noopener noreferrer">
+          Ir a la versión de escritorio
+        </a>
       </div>
     </div>
   );
