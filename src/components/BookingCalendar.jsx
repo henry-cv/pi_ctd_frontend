63189f9000619035143d3fdfc9d0f9507709681e
vioru <vioru@live.com>
2025-03-11 16:01:26 -0500

    Cambios a calendario sencillo y estilos de mobile

--- src/components/BookingCalendar.jsx
+++ src/components/BookingCalendar.jsx
@@ -5,41 +5,54 @@ import "react-date-range/dist/theme/default.css";
 import { useState } from "react";
 import "../css/components/BookingCalendar.css"
 import { useContextGlobal } from "../gContext/globalContext";
+import { es } from 'date-fns/locale';
 
 const BookingCalendar = ({ anchorEl, handleClose, dateRange, setDateRange }) => {
   const isMobile = useMediaQuery("(max-width: 480px)");
   const [selectedDate, setSelectedDate] = useState(dateRange[0]?.startDate);
+  const [visibleMonth, setVisibleMonth] = useState(new Date().getMonth());
+  const [visibleYear, setVisibleYear] = useState(new Date().getFullYear());
   const { state } = useContextGlobal();
 
 
-  const getDayClass = (day) => {
-    const dayOfWeek = day.getDay();
-
-    if (selectedDate?.toDateString() === day.toDateString()) return "selected-day"; // el dia que queda seleccionado
-    if (dayOfWeek === 0) return "disabled-day"; // osea el domingo
-    if (dayOfWeek === 3 || dayOfWeek === 4 || dayOfWeek === 5) return "available-day"; // Miércoles a Viernes 
-    return "semi-available-day"; // Otros días aun no sé si lo voy a usar talvez para el tachado
+  const handleMonthChange = (date) => {
+    setVisibleMonth(date.getMonth());
+    setVisibleYear(date.getFullYear());
   };
-
+  
+  const getDayClass = (day) => {
+    const isOutsideMonth = day.getMonth() !== visibleMonth || day.getFullYear() !== visibleYear;
+  
+    if (isOutsideMonth) return ""; // No aplicamos ninguna clase si no es del mes visible
+    const dayOfWeek = day.getDay();
+  
+    if (selectedDate?.toDateString() === day.toDateString()) return "selected-day"; // Día seleccionado
+    if (dayOfWeek === 0) return "disabled-day"; // Domingo como día no seleccionable
+    if (dayOfWeek === 3 || dayOfWeek === 4 || dayOfWeek === 5) return "available-day"; // Miércoles a Viernes
+    return "semi-available-day"; // Otros días
+  };
   return (
     <Popover
       open={Boolean(anchorEl)}
       anchorEl={anchorEl}
       onClose={handleClose}
+      disableEnforceFocus
       anchorOrigin={{
         vertical: "bottom",
         horizontal: "left",
       }}
     >
       <Box p={2} className={state.theme === "dark" ? "CardBooking-container" : ""}>
-        <DateRange className={state.theme === "dark" ? "CardBooking-container" : ""}
+        <DateRange className={`${state.theme === "dark" ? "CardBooking-calendar" : ""} ${isMobile ? "mobile-calendar " : ""}`}
+              locale={es} 
           ranges={dateRange}
           onChange={(ranges) => {
             setDateRange([ranges.selection]);
             setSelectedDate(ranges.selection.startDate);
             handleClose();
           }}
-          months={isMobile ? 1 : 2}
+          onShownDateChange={(date) => handleMonthChange(date)}
+          months={1}
           direction={isMobile ? "vertical" : "horizontal"}
           showDateDisplay={false}
           dayContentRenderer={(date) => (
@@ -48,7 +61,7 @@ const BookingCalendar = ({ anchorEl, handleClose, dateRange, setDateRange }) => 
         />
 
     
-        <Typography >
+        <Typography  className={isMobile ? "mobile-calendar-legend" : ""}>
           <ul>
             <li>Disponible</li>
             <li>Cupo lleno</li>
--- src/components/BookingModal.jsx
+++ src/components/BookingModal.jsx
@@ -13,6 +13,8 @@ import {
   FormControlLabel,
   Radio,
   Checkbox,
+  useMediaQuery,
+  Tooltip,
 } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
@@ -21,12 +23,14 @@ import PlaceIcon from "@mui/icons-material/Place";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import { format } from "date-fns";
 import BookingCalendar from "./BookingCalendar";
-import { IoCalendarClear, IoRadioButtonOff } from "react-icons/io5";
-import { FaCreditCard } from "react-icons/fa";
+import { IoCalendarClear, IoGameController, IoRadioButtonOff, IoTicket } from "react-icons/io5";
+import { FaCreditCard, FaInfo } from "react-icons/fa";
 import ButtonBluePill from "./ButtonBluePill";
+import "../css/components/BookingModal.css"
 
 const BookingModal = ({ open, handleClose, activityId }) => {
   const [quantity, setQuantity] = useState(1);
+  const isMobile = useMediaQuery("(max-width: 480px)");
   const [dateRange, setDateRange] = useState([
     {
       startDate: new Date(),
@@ -35,13 +39,11 @@ const BookingModal = ({ open, handleClose, activityId }) => {
     },
   ]);
   const [anchorEl, setAnchorEl] = useState(null);
+  const [priceQuantity , setPriceQuantity] = useState(0) //para guardar luego el precio total ese se iria a reserva 
   const pricePerPerson = 75;
   const total = quantity * pricePerPerson;
 
-
-  console.log("llegó el id "+activityId);
-  //traer la info del producto con el fetch o guardar en el global y traer aqui luego la info
-  
+  console.log("llegó el id " + activityId);
 
   const handleOpenCalendar = (event) => {
     setAnchorEl(event.currentTarget);
@@ -51,9 +53,25 @@ const BookingModal = ({ open, handleClose, activityId }) => {
     setAnchorEl(null);
   };
 
+  const handleOpenBookingQuantity = () => {
+    console.log("abre el modal cantidad");
+  };
+
+  const BookingSubmit = ()=>{
+    console.log("hacer el submit de la reserva ");
+
+  }
+
   return (
-    <Dialog open={open} onClose={handleClose} maxWidth="sm" fullWidth>
-      <DialogTitle>
+    <Dialog 
+      open={open} 
+      onClose={handleClose} 
+      maxWidth="sm" 
+      fullWidth  
+      sx={{ "& .MuiPaper-rounded": { borderRadius: "25px" } }}
+
+    >
+      <DialogTitle color="black" >
         Tour en el centro amurallado
         <IconButton
           aria-label="close"
@@ -65,11 +83,10 @@ const BookingModal = ({ open, handleClose, activityId }) => {
       </DialogTitle>
       <DialogContent>
         <Typography variant="body2">
-          Esta actividad es por persona. Selecciona cuántas reservas deseas.
+        Esta actividad se reserva por persona. Selecciona la fecha y cuántos cupos  deseas .
         </Typography>
 
         <Box display="flex" alignItems="center" mt={2} gap={1}>
-
           <IconButton onClick={handleOpenCalendar}>
             <CalendarTodayIcon />
           </IconButton>
@@ -81,32 +98,34 @@ const BookingModal = ({ open, handleClose, activityId }) => {
             {format(dateRange[0].startDate, "EEE, dd MMM")}
           </Typography>
 
+          
+            <Box display="flex" alignItems="center" gap={1}>
+              <IconButton onClick={handleOpenBookingQuantity}>
+                <IoTicket />
+              </IconButton>
+              <Typography
+                variant="body2"
+                sx={{ cursor: "pointer" }}
+                onClick={handleOpenBookingQuantity}
+              >
+                Selecciona tus cupos
+              </Typography>
+            </Box>
  
-          <PersonIcon />
-          <Select
-            value={quantity}
-            onChange={(e) => setQuantity(e.target.value)}
-            size="small"
-          >
-            {[1, 2, 3, 4, 5].map((num) => (
-              <MenuItem key={num} value={num}>
-                {num}
-              </MenuItem>
-            ))}
-          </Select>
         </Box>
 
-     
         <BookingCalendar
           anchorEl={anchorEl}
           handleClose={handleCloseCalendar} 
           dateRange={dateRange}
           setDateRange={setDateRange}
         />
+        
+        {/* aqui se pone si hay menos de 3 reservas  */}
 
-        <Typography color="error" mt={1}>
+        {/* <Typography color="error" mt={1}>
           ¡Últimas 3 reservas disponibles para esta fecha!
-        </Typography>
+        </Typography> */}
 
         <Box bgcolor="#f1c40f" p={2} mt={2} borderRadius={2}>
           <Typography fontWeight={600}>
@@ -126,13 +145,12 @@ const BookingModal = ({ open, handleClose, activityId }) => {
           </Typography>
         </Box>
 
-
-        <Box mt={3} p={2} border={1} borderRadius={2}>
-          <div >
-          <Typography fontWeight={600}>Tour en el centro amurallado</Typography>
-          <FormControlLabel control={<Checkbox/>}/>
+        <Box mt={3} p={2} border={1} borderRadius={2} className="Card-info-booking">
+          <div className="title-checkbox">
+            <Typography fontWeight={600}>Tour en el centro amurallado</Typography>
+            <FormControlLabel control={<Checkbox />} />
           </div>
-         
+
           <Box display="flex" alignItems="center" mt={1} gap={1}>
             <PlaceIcon />
             <Typography>Cartagena, Colombia</Typography>
@@ -149,16 +167,13 @@ const BookingModal = ({ open, handleClose, activityId }) => {
         </Box>
       </DialogContent>
 
-      <DialogActions>
-                <ButtonBluePill
-                  text= "Reservar Ahora"
-                  className="button-blue btn-save"
-                  type="submit"
-                  
-                />
-        {/* <Button variant="contained" fullWidth sx={{ bgcolor: "#7D5FFF" }}>
-          Reservar ahora
-        </Button> */}
+      <DialogActions className="Booking-principal-container">
+        <ButtonBluePill
+          text="Reservar Ahora"
+          className="button-blue btn-save"
+          type="submit"
+          onClick ={BookingSubmit}
+        />
       </DialogActions>
     </Dialog>
   );
