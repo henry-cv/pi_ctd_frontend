aaff8e797eab2d362996f0739b0b92b12c86dbca
henry-cv <thechosenone.cc@gmail.com>
2025-03-13 12:50:10 -0500

    agregado estilos bold a elemento seleccionado en aside

--- src/components/AsidePayments.jsx
+++ src/components/AsidePayments.jsx
@@ -4,16 +4,19 @@ import PropTypes from 'prop-types';
 
 
 const AsidePayments = ({ setSelectedPolicy }) => {
-  const [activeAccordion, setActiveAccordion] = useState(null);
-  const [selectedTitle, setSelectedTitle] = useState(null);
+  const [activeAccordion, setActiveAccordion] = useState(-1);
+  const [selectedTitle, setSelectedTitle] = useState("");
+  const [selectedPolicyIndex, setSelectedPolicyIndex] = useState(-1);
 
   const handleAccordionClick = (index, title) => {
     setActiveAccordion(prevIndex => prevIndex === index ? null : index);
     setSelectedTitle(title);
   };
   //Manejador de política, establece la política según el elemento clicado
-  const handlePolicyClick = (policy) => {
+  const handlePolicyClick = (policy, index) => {
     setSelectedPolicy(policy);
+    setSelectedPolicyIndex(index);
+
   };
   return (
     <aside className="sidebar-policies">
@@ -23,7 +26,7 @@ const AsidePayments = ({ setSelectedPolicy }) => {
           <h5 className={selectedTitle === 'planificar' ? 'title-selected' : ''}>Planificar, buscar y reservar <span className="caret"></span></h5>
 
           {activeAccordion === 1 && (
-            <div className="sub-accordion">
+            <div className="sub-accordion" onClick={(e) => e.stopPropagation()}>
               <p>Planificar</p>
               <p>Buscar</p>
               <p>Reservar</p>
@@ -33,9 +36,15 @@ const AsidePayments = ({ setSelectedPolicy }) => {
         <div className="accordion-item" onClick={() => handleAccordionClick(2, 'pagos')}>
           <h5 className={selectedTitle === 'pagos' ? 'title-selected' : ''} onClick={() => handlePolicyClick('pagos')}>Pagos <span className="caret"></span></h5>
           {activeAccordion === 2 && (
-            <div className="sub-accordion">
-              <p onClick={() => handlePolicyClick('pagoInmediato')}>Pago inmediato y reserva confirmada</p>
-              <p onClick={() => handlePolicyClick('reserveAhora')}>Reserve ahora, pague después</p>
+            <div className="sub-accordion" onClick={(e) => e.stopPropagation()}>
+              <p onClick={() =>
+                handlePolicyClick('pagoInmediato', 0)
+              } className={selectedPolicyIndex === 0 ? 'selected-policy' : ''}
+              >Pago inmediato y reserva confirmada</p>
+              <p onClick={() =>
+                handlePolicyClick('reserveAhora', 1)
+              } className={selectedPolicyIndex === 1 ? 'selected-policy' : ''}
+              >Reserve ahora, pague después</p>
             </div>
           )}
         </div>
@@ -43,10 +52,28 @@ const AsidePayments = ({ setSelectedPolicy }) => {
           <h5 className={selectedTitle === 'cancelaciones' ? 'title-selected' : ''} onClick={() => handlePolicyClick('cancelaciones')}>Cancelaciones <span className="caret"></span></h5>
 
           {activeAccordion === 3 && (
-            <div className="sub-accordion">
-              <p onClick={() => handlePolicyClick('reembolso7dias')} >Reembolso total hasta 7 días antes</p>
-              <p onClick={() => handlePolicyClick('reembolso24horas')} >Reembolso total hast 25 horas antes</p>
-              <p onClick={() => handlePolicyClick('noReembolsable')} >No Reembolsable</p>
+            <div className="sub-accordion" onClick={(e) => e.stopPropagation()}>
+              {/* <p onClick={() => handlePolicyClick('reembolso7dias')} >Reembolso total hasta 7 días antes</p>
+              <p onClick={() => handlePolicyClick('reembolso24horas')} >Reembolso total hasta 24 horas antes</p>
+              <p onClick={() => handlePolicyClick('noReembolsable')} >No Reembolsable</p> */}
+              <p
+                onClick={() => handlePolicyClick('reembolso7dias', 0)}
+                className={selectedPolicyIndex === 0 ? 'selected-policy' : ''}
+              >
+                Reembolso total hasta 7 días antes
+              </p>
+              <p
+                onClick={() => handlePolicyClick('reembolso24horas', 1)}
+                className={selectedPolicyIndex === 1 ? 'selected-policy' : ''}
+              >
+                Reembolso total hasta 24 horas antes
+              </p>
+              <p
+                onClick={() => handlePolicyClick('noReembolsable', 2)}
+                className={selectedPolicyIndex === 2 ? 'selected-policy' : ''}
+              >
+                No Reembolsable
+              </p>
             </div>
           )}
         </div>
