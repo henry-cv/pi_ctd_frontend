02877e3523c34a30389d764273ce61d3a02b913e
Bruno Collazo <141088902+BrunoCollazo@users.noreply.github.com>
2025-04-03 06:07:49 -0300

    Fix: Import Use Memo

--- src/hooks/useReviews.js
+++ src/hooks/useReviews.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { useContextGlobal } from "../gContext/globalContext";
 
 export function useReviews(productoId) {
